add6a3bc07a4f4efae6c766c18555410325f9a81
Ayomide <ayomidebabarinde07@gmail.com>
2026-05-04 07:54:52 -0700

    Remove unsupported mongoose option bufferMaxEntries

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -13,7 +13,6 @@ const connectDB = async () => {
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
       bufferCommands: false, // Disable mongoose buffering
-      bufferMaxEntries: 0, // Disable mongoose buffering
       maxPoolSize: process.env.NODE_ENV === "production" ? 100 : 10,
       minPoolSize: process.env.NODE_ENV === "production" ? 10 : 1,
       maxIdleTimeMS: 30000,
